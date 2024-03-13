c7886ebaaa4b323820923a05932c81322782ab8c
Remi <remi.k.work@gmail.com>
2024-03-13 22:40:46 +0100

    rwd header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,14 +17,11 @@ import { lusitana } from "@/assets/fonts";
 
 export default function Header({ cart }) {
   return (
-    <header className={clsx(styles["header"], "navbar z-30 gap-4")}>
+    <header className={clsx(styles["header"], "navbar z-30 flex-wrap gap-4")}>
       <div className="flex-1">
         <Link href={"/"}>
-          <h1 className={clsx(lusitana.className, "text-5xl")}>NoLine-Deli</h1>
+          <h1 className={clsx(lusitana.className, "text-4xl lg:text-5xl")}>NoLine-Deli</h1>
         </Link>
-      </div>
-      <div className="flex-none">
-        <SearchPanel />
       </div>
       <div className="flex-none">
         <CartIndicator cart={cart} />
@@ -33,6 +30,9 @@ export default function Header({ cart }) {
         <label htmlFor="navBar" className="btn btn-square btn-ghost drawer-button">
           <Bars4Icon width={24} height={24} />
         </label>
+      </div>
+      <div className="flex-none">
+        <SearchPanel />
       </div>
     </header>
   );
